ae9d4ddc184d5b11b6ef3464917749dd9aa9bb23
Justin Singh <justin.singh125125@gmail.com>
2019-07-24 15:12:03 -0700

    Building the Homes Section - Part 1

--- src/components/HomeCard/index.js
+++ src/components/HomeCard/index.js
@@ -7,7 +7,47 @@ const HomeCard = props => {
     return (
         <div className="home">
             <img src={props.image} alt={props.alt} className="home__img" />
-            <SvgIcon />
+            <SvgIcon
+                svgClass="home_like"
+                svgSelection="icon-heart-full"
+            />
+
+            <h5 className="home__name">{props.headingText}</h5>
+
+            <div className="home__location">
+                <SvgIcon
+                    svgSelection="icon-map-pin"
+                />
+
+                <p>{props.locationText}</p>
+            </div>
+
+
+            <div className="home__rooms">
+                <SvgIcon
+                    svgSelection="icon-profile-male"
+                />
+
+                <p>{props.roomsText}</p>
+            </div>
+
+
+            <div className="home__area">
+                <SvgIcon
+                    svgSelection="icon-profile-male"
+                />
+                <p>{props.areaText_1}<sup>{props.areaText_2}</sup></p>
+            </div>
+
+            <div className="home__price">
+                <SvgIcon
+                    svgSelection="icon-key"
+                />
+                <p>{props.priceText}</p>
+            </div>
+
+            <button className="cta">Contact Realtor</button>
+
         </div>
     )
 }
